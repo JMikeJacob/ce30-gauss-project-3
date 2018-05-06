66578f63ec710852f47b5a13441ecaf32687ce4a
nicamedrano <37525942+nicamedrano@users.noreply.github.com>
2018-05-06 19:52:05 +0800

    nilagay ko na save to file haha

--- main.cpp
+++ main.cpp
@@ -86,5 +86,53 @@ int main(int argc, char *argv[])
   
   GaussMatrix.deleteArrays();
   return 0;
-  
+   //start of save to file part
+  int rows = 3, cols = 4;
+    double matrix[rows][cols] = {{1,2,3,4}, {5,6,7,8},{9,10,11,12}};
+    string save;
+    int setS, num;
+    string saveToFile;
+    cout << "Do you want to save the results to a text file? (Yes or No?)\nAnswer: ";
+    cin>> saveToFile;
+    cin.clear();
+    if (saveToFile=="yes" || saveToFile=="Yes")
+    {
+      cout << "To which text file would you want to save your data?"<< endl;
+      cout<< "Filename: ";
+      cin >> save;
+      cin.clear();
+      if(save.find(".txt") == string::npos) //adds file extension
+      {
+        save = save + ".txt";
+      }
+      ofstream data;
+      data.open(save);
+      if(data.is_open()==true)
+      {
+        cout<<"How many significant digits do you like in presenting your data?"<< endl;
+        cout<< "Answer: ";
+        cin>> setS;
+        cin.clear();
+        for(int i=0; i<rows; i++)
+        {
+            for(int j=0; j<cols; j++)
+            {
+                printDouble(data,matrix[i][j], setS);
+                data<< " ";
+            }
+            data<< endl;
+        }
+      }
+      else
+      {
+        cout<<"Error in loading file!"<< endl;
+      }
+
+    }
+
+    else if (saveToFile=="no" || saveToFile=="No")
+    {
+        return 0;
+    }
+    //end of savetofile part
 }
